9c5e5abc27562cea393e8f95cc280a56a02915a2
Kailash Chandrasekaran <kailash.chandrasekaran@immutable.com>
2022-05-24 02:43:12 +1000

    Make stoplight work

--- docs/guides/asset-management/asset-deposits.md
+++ docs/guides/asset-management/asset-deposits.md
@@ -8,7 +8,7 @@ An alternative to L2 minting assets, assuming you have pre minted assets on Ethe
 As a **pre-requisite**, ensure that both the contract and the user depositing has been registered in our system.
 
 The diagram below is a high-level overview of the asset depositing process.
-![Asset Deposits](../../../static/img/Asset%20Deposits.png)
+![Asset Deposits](../../../static/img/AssetDeposits.png)
 
 We make 2 client-side calls to abstract much of the unnecessary jargon from you, the call to [Gets details of a signable deposit](ref:post_v1-signable-deposit-details-1) is necessary to query your L2 key given your L1 key, allocate a new vault be it a new asset and convert the asset into an identifier used within our system.
 
--- src/pages/reference.tsx
+++ src/pages/reference.tsx
@@ -1,17 +1,48 @@
-import React from 'react'
-import { API } from '@stoplight/elements'
-import '@stoplight/elements/styles.min.css'
-import Layout from '@theme/Layout'
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import React from 'react';
+import Head from '@docusaurus/Head';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
-export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext()
+import Layout from '@theme/Layout';
+
+export default function APIPage() {
   return (
-    <Layout
-      title="Immutable X API Reference"
-      description="Reference for the Immutable X API"
-    >
-      <API apiDescriptionUrl="https://raw.githubusercontent.com/stoplightio/Public-APIs/master/reference/zoom/openapi.yaml" />
+    <Layout>
+      <Head>
+        <title>API Reference | Immutable X Documentation</title>
+        <meta name="description" content="Immutable X REST API Reference" />
+        <meta name="og:description" content="Immutable X REST API Reference" />
+        {/* Loading styles for elements this way so it doesn't interfere with other styles */}
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/@stoplight/elements@7.3.7/styles.min.css"
+        />
+      </Head>
+      <BrowserOnly
+        fallback={
+          <div className="flex min-h-screen w-full items-center justify-center">
+            <div
+              className="h-10 w-10 animate-spin rounded-full border-l border-t-2 border-primary"
+              aria-label="Loading..."
+            ></div>
+          </div>
+        }
+      >
+        {() => {
+          // eslint-disable-next-line no-undef
+          const { API } = require('@stoplight/elements');
+          return (
+            <>
+              <API
+                apiDescriptionUrl="https://api.ropsten.x.immutable.com/openapi"
+                router="hash"
+                basePath="/"
+                layout="sidebar"
+                hideSchemas
+              />
+            </>
+          );
+        }}
+      </BrowserOnly>
     </Layout>
-  )
+  );
 }
