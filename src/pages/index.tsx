479a7bc8a91b7b3dd2dcfe73b22deb16cab69fc3
Kailash Chandrasekaran <kailash.chandrasekaran@immutable.com>
2022-06-01 17:09:21 +1000

    fix(stray link): stray link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -92,7 +92,7 @@ const Homepage = () => {
       LightIcon: ContractLightIcon,
       DarkIcon: ContractDarkIcon,
       title: 'Code Examples',
-      subtitle: '“Accelerate your build with code snippets and samples.',
+      subtitle: 'Accelerate your build with code snippets and samples.',
       buttonText: 'Learn More',
       link: '/code-samples',
     },
