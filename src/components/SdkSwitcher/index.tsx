c6c7b5b7d30ca9df187daf15e9c934da1031fb0a
Nik Ho <studying.mole@gmail.com>
2022-07-29 11:51:51 +1200

    refactor: disable Tailwind CSS

--- src/components/SdkSwitcher/index.tsx
+++ src/components/SdkSwitcher/index.tsx
@@ -46,17 +46,17 @@ const SdkSwitcher = () => {
               Core SDK
             </Listbox.Label>
             <div className="mt-1 relative">
-              <Listbox.Button className="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-cyan-500 focus:border-cyan-500 sm:text-sm">
-                <span className="flex items-center">
-                  <span className="ml-3 block truncate">
+              <Listbox.Button className={clsx(styles.switcherButton)}> { /* "relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-cyan-500 focus:border-cyan-500 sm:text-sm" */ }
+                <span className={clsx(styles.switcherButtonContent)}> { /* "flex items-center" */ }
+                  <span className={clsx(styles.switcherButtonText)} > { /* "ml-3 block truncate" */ }
                     {selectedSdk.name}
                   </span>
                 </span>
-                <span className="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                <span className={clsx(styles.switcherButtonIconGroup)}> { /* "ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none" */ }
                   <SelectorIcon
-                    className="h-5 w-5 text-gray-400"
+                    className={clsx(styles.switcherButtonIcon)}
                     aria-hidden="true"
-                  />
+                  /> { /* "h-5 w-5 text-gray-400" */ }
                 </span>
               </Listbox.Button>
 
@@ -67,46 +67,55 @@ const SdkSwitcher = () => {
                 leaveFrom="opacity-100"
                 leaveTo="opacity-0"
               >
-                <Listbox.Options className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+                {/* ul */}
+                <Listbox.Options className={clsx(styles.switcherList)}> { /* "absolute z-10 mt-1 w-full bg-white shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm" */ }
                   {sdks.map((sdk) => (
-                    <Listbox.Option
+                    <Listbox.Option // li
                       key={sdk.id}
-                      className={({ active }) =>
-                        classNames(
-                          active ? 'text-white bg-cyan-600' : 'text-gray-900',
-                          'cursor-default select-none relative py-2 pl-3 pr-9'
-                        )
-                      }
+                      // className={({ active }) =>
+                      //   classNames(
+                      //     active ? 'text-white bg-cyan-600' : 'text-gray-900',
+                      //     'cursor-default select-none relative py-2 pl-3 pr-9'
+                      //   )
+                      // }
+                      className={clsx(styles.switcherListOption)}
                       value={sdk}
                     >
                       {({ selected, active }) => {
+                        console.log(selected);
                         return (
-                          <>
-                            <div className="flex items-center">
+                          // <>
+                            <div className={clsx(styles.switcherListOptionContent)}> { /* "flex items-center" */ }
                               <span
-                                className={classNames(
-                                  selected ? 'font-semibold' : 'font-normal',
-                                  'ml-3 block truncate'
-                                )}
+                                // className={classNames(
+                                //   selected ? 'font-semibold' : 'font-normal',
+                                //   'ml-3 block truncate'
+                                // )}
+                                className={
+                                  selected 
+                                  ? clsx(styles.switcherListOptionTextSelected)
+                                  : clsx(styles.switcherListOptionText)
+                                }
                               >
                                 {sdk.name}
                               </span>
-                            </div>
 
-                            {selected ? (
+                              {selected ? (
                               <span
-                                className={classNames(
-                                  active ? 'text-white' : 'text-indigo-600',
-                                  'absolute inset-y-0 right-0 flex items-center pr-4'
-                                )}
+                                // className={classNames(
+                                //   active ? 'text-white' : 'text-indigo-600',
+                                //   'absolute inset-y-0 right-0 flex items-center pr-4'
+                                // )}
+                                className={clsx(styles.switcherListOptionSelected)}
                               >
                                 <CheckIcon
-                                  className="h-5 w-5"
+                                  className={clsx(styles.switcherListOptionSelectedCheck)}
                                   aria-hidden="true"
-                                />
+                                />{ /* "h-5 w-5" */}
                               </span>
                             ) : null}
-                          </>
+                            </div>
+                          //{/* </> */}
                         );
                       }}
                     </Listbox.Option>
