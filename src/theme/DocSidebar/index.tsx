9045c8e5beb965de6466809f1c9ad6bc97d86efe
Nik Ho <studying.mole@gmail.com>
2022-07-28 16:15:50 +1200

    fix: style fix

--- src/theme/DocSidebar/index.tsx
+++ src/theme/DocSidebar/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import BrowserOnly from '@docusaurus/BrowserOnly';
 import DocSidebar from '@theme-original/DocSidebar';
-import SdkSwitcher from '@site/src/components/SdkSwitcher';
+// import SdkSwitcher from '@site/src/components/SdkSwitcher';
 import styles from './styles.module.css';
 
 interface SidebarProps {
@@ -9,13 +9,13 @@ interface SidebarProps {
 }
 
 const DocSidebarWrapper = (props: SidebarProps) => {
-  const isSdkDocsPath = props.path.startsWith('/sdk-docs/') || false;
+  // const isSdkDocsPath = props.path.startsWith('/sdk-docs/') || false;
 
   return (
     <BrowserOnly>
       {() => (
         <div className={styles.sidebar}>
-          {isSdkDocsPath && <SdkSwitcher />}
+          {/* {isSdkDocsPath && <SdkSwitcher />} */}
           <DocSidebar {...props} />
         </div>
       )}
