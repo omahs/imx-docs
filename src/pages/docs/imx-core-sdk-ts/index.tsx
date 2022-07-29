4d01dbb09ad9f9e605058583ab15562432c1d44d
Nik Ho <studying.mole@gmail.com>
2022-07-29 18:40:22 +1200

    fix: redirect to TS SDK overview - remove code flash

--- src/pages/docs/imx-core-sdk-ts/index.tsx
+++ src/pages/docs/imx-core-sdk-ts/index.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { Redirect } from '@docusaurus/router';
 
 export default function ImxCoreSdkTs() {
-  return <Redirect to="/sdk-docs/core-sdk-ts/" />;
+  return <Redirect to="/sdk-docs/core-sdk-ts/overview" />;
 }
