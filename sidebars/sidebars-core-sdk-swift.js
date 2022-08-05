a953b02ea6660f894a10f588c28877a5e8cb3b8d
Cassius Pacheco <cassius.pacheco@immutable.com>
2022-08-05 16:47:06 +1000

    docs: include swift static docs
    NOTE: Apple's Swift DocC appends /documentation/immutablexcore to the
    url

--- sidebars/sidebars-core-sdk-swift.js
+++ sidebars/sidebars-core-sdk-swift.js
@@ -10,6 +10,11 @@ module.exports = {
         'sdk-functions',
         'additional-info',
         'changelog',
+        {
+          type: 'link',
+          label: 'Reference',
+          href: 'https://docs.x.immutable.com/sdk-references/core-sdk-swift/0-1-0/documentation/immutablexcore',
+        },
       ],
     },
   ],
