542bcb9c724a3b4b47c53c6458999b1411ffaeff
Adora <96668470+sprngbk@users.noreply.github.com>
2022-09-29 12:22:07 +1000

    docs: remove ref to unused component and fix linter issues

--- docs/main/guides-new/get-data/guideContent.js
+++ docs/main/guides-new/get-data/guideContent.js
@@ -1,44 +1,44 @@
 export default {
   getProjects: [
     {
-      value: "typescript",
-      label: "Typescript Core SDK",
-      desc: "Add description or explanation for usage of Typescript Core SDK, if required.",
+      value: 'typescript',
+      label: 'Typescript Core SDK',
+      desc: 'Add description or explanation for usage of Typescript Core SDK, if required.',
       code: `// Add code
       // Add more code
-      `
+      `,
     },
     {
-      value: "kotlin-jvm",
-      label: "Kotlin (JVM) Core SDK",
-      desc: "Add description or explanation for usage of Kotlin (JVM) Core SDK, if required.",
+      value: 'kotlin-jvm',
+      label: 'Kotlin (JVM) Core SDK',
+      desc: 'Add description or explanation for usage of Kotlin (JVM) Core SDK, if required.',
       code: `// Add code
       // Add more code
-      `
+      `,
     },
     {
-      value: "swift",
-      label: "Swift Core SDK",
-      desc: "Add description or explanation for usage of Swift Core SDK, if required.",
+      value: 'swift',
+      label: 'Swift Core SDK',
+      desc: 'Add description or explanation for usage of Swift Core SDK, if required.',
       code: `// Add code
       // Add more code
-      `
+      `,
     },
     {
-      value: "golang",
-      label: "Golang Core SDK",
-      desc: "Add description or explanation for usage of Golang Core SDK, if required.",
+      value: 'golang',
+      label: 'Golang Core SDK',
+      desc: 'Add description or explanation for usage of Golang Core SDK, if required.',
       code: `// Add code
       // Add more code
-      `
+      `,
     },
     {
-      value: "starknet",
-      label: "StarkNet",
-      desc: "Add description or explanation for building on StarkNet settlement layer, if required.",
+      value: 'starknet',
+      label: 'StarkNet',
+      desc: 'Add description or explanation for building on StarkNet settlement layer, if required.',
       code: `// Add code
       // Add more code
-      `
-    }
-  ]
+      `,
+    },
+  ],
 };
