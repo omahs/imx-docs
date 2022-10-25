86341269e2d2b10f8252f7f8cc592c5ca24db643
Aung T. T. Kyaw <zatlite@gmail.com>
2022-10-25 16:07:28 +1100

    docs(core-sdk-ts): add examples sidebar link (#152)
    [DX-1308]

--- sidebars/sidebars-core-sdk-ts.js
+++ sidebars/sidebars-core-sdk-ts.js
@@ -14,6 +14,11 @@ module.exports = {
         'changelog',
         {
           type: 'link',
+          label: 'Examples',
+          href: `https://github.com/immutable/imx-core-sdk/tree/827da9438d3be96852c8391b5a44950f4582a1be/examples`,
+        },
+        {
+          type: 'link',
           label: 'Reference',
           href: `${url}/sdk-references/core-sdk-ts/1.0.0-beta.3/classes/ImmutableX.ImmutableX.html`,
         },
