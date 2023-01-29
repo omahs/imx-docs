ce00b9077595c809de5eba99ec5937482c809c59
Adora <96668470+sprngbk@users.noreply.github.com>
2023-01-30 10:18:18 +1100

    docs: add sdk reference docs

--- sidebars/sidebars-core-sdk-csharp.js
+++ sidebars/sidebars-core-sdk-csharp.js
@@ -12,6 +12,11 @@ module.exports = {
         "additional-info",
         {
           type: 'link',
+          label: 'Reference',
+          href: `${url}/sdk-references/core-sdk-csharp/0-1-0/docs`,
+        },
+        {
+          type: 'link',
           label: 'Github',
           href: `https://github.com/immutable/imx-core-sdk-csharp`,
         },
